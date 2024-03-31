6e2b8d53e175b06fab839b5aab8495d45f5ec315
Vita Golubovska <143090166+Golubko@users.noreply.github.com>
2024-03-31 02:24:20 +0200

    Added support for dark mode

---
+++ request_validators.py
@@ -0,0 +1,3 @@
+Contains(Sphere(lss.mP0, lss.mRadius)) &&
+Well, to be frank, I do not have a complete answer. At least, I never read about such situations in books but often came across consequences of small Copy-Paste's in practice, including my own applications. So I will have to improvise while answering the question.
+public class PalindromeChecker {
